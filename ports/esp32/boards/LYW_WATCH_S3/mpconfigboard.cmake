c3da13af9dc70e0d0d049aafc29d0051bbbac0a0
yuwen Luo <2410048354@qq.com>
2026-03-15 18:11:44 +0800

    Update mpconfigboard.cmake
    Signed-off-by: yuwen Luo <2410048354@qq.com>

--- ports/esp32/boards/LYW_WATCH_S3/mpconfigboard.cmake
+++ ports/esp32/boards/LYW_WATCH_S3/mpconfigboard.cmake
@@ -1,2 +1,3 @@
 set(IDF_TARGET esp32s3)
-set(SDKCONFIG_DEFAULTS boards/sdkconfig.base boards/sdkconfig.usb_disabled)
+# 移除那个不存在的 usb_disabled 文件引用，只引用基础配置
+set(SDKCONFIG_DEFAULTS boards/sdkconfig.base)
