521c08069a7b8eb935e0b66c9854a1e1342baf28
yuwen Luo <2410048354@qq.com>
2026-03-15 22:12:22 +0800

    Update mpconfigboard.h
    Signed-off-by: yuwen Luo <2410048354@qq.com>

--- ports/esp32/boards/LYW_WATCH_S3/mpconfigboard.h
+++ ports/esp32/boards/LYW_WATCH_S3/mpconfigboard.h
@@ -1,10 +1,11 @@
 #define MICROPY_HW_BOARD_NAME               "LYW_WATCH_S3"
 #define MICROPY_HW_MCU_NAME                 "ESP32S3"
 
+// 启用 UART REPL (配合 CP2104)
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
+
+// 彻底禁用底层的 USB 和 JTAG
 #define MICROPY_HW_ESP_USB_SERIAL_JTAG      (0)
 #define MICROPY_HW_ENABLE_USBDEV            (0)
-
-/* 下面这些不是必须，但保留无妨 */
 #define MICROPY_HW_USB_CDC                  (0)
 #define MICROPY_HW_USB_MSC                  (0)
